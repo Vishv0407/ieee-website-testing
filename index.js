932f5292c172a3abcf788a9b46e1c71c19c815fd
Vishv0407 <vishvkumar.b@ahduni.edu.in>
2024-07-16 14:34:41 +0530

    something

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
 const nodemailer = require('nodemailer');
-const { env } = require('process');
 require('dotenv').config();
 
 const app = express();
